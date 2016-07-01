2c43dd5f101b3b771cd21c31c07a8345f43fa01f
Fred Cox <fred@ekreative.com>
2016-07-01 15:15:37 +0300

    Fix creating IPA on yosemite
    Builds are already signed so no need to resign
    add-keys supports list of profiles, so also reading the env_var as a
    list here
    No need to use the installed version of profile, raw value works

--- create-ipa.js
+++ create-ipa.js
@@ -7,33 +7,25 @@ const winston = require('winston'),
     os = require('os'),
 
     exec = require('./lib/exec'),
-    find = require('./lib/find-identity-name');
+    list = require('./lib/list');
 
 program
     .version(require('./package.json').version)
     .description('Create an .ipa file from an .app')
     .option('-k, --keychain-name <name>', 'Keychain Name - default APP_NAME', process.env.APP_NAME || 'build-tools')
-    .option('--developer-name <name>', 'Developer name to use - CODE_SIGN_IDENTITY', process.env.CODE_SIGN_IDENTITY || process.env.DEVELOPER_NAME)
     .option('--ipa <name>', 'Ipa file to create - default build/Release-iphoneos/$APP_NAME.ipa', `${process.cwd()}/build/Release-iphoneos/` + (process.env.APP_NAME ? `${process.env.APP_NAME}.ipa` : 'app.ipa'))
     .option('--app <name>', 'App file to convert - default build/Release-iphoneos/$APP_NAME.app', `${process.cwd()}/build/Release-iphoneos/` + (process.env.APP_NAME ? `${process.env.APP_NAME}.app` : 'app.app'))
-    .option('--provisioning-profile <profile>', 'Provisioning profile - default PROVISIONING_PROFILE', process.env.PROVISIONING_PROFILE)
+    .option('--provisioning-profile <profile>', 'Provisioning profile - default PROVISIONING_PROFILE', list, list(process.env.PROVISIONING_PROFILE))
     .parse(process.argv);
 
-let create = (developerName) => {
-    let name = path.basename(program.provisioningProfile, path.extname(program.provisioningProfile));
+let create = () => {
+    let embed = program.provisioningProfile.length ? ` -embed "${program.provisioningProfile}"` : '';
 
-    return exec(`xcrun -log -sdk iphoneos PackageApplication "${program.app}" -o "${program.ipa}" -sign "${developerName}" -embed "${os.homedir()}/Library/MobileDevice/Provisioning Profiles/${name}.mobileprovision"`);
+    return exec(`xcrun -log -sdk iphoneos PackageApplication "${program.app}" -o "${program.ipa}" ${embed}`);
 };
 
-let commandPromise;
-
-if (program.developerName) {
-    commandPromise = create(program.developerName);
-} else {
-    commandPromise = find(program.keychainName).then(create);
-}
-
-commandPromise.catch((err) => {
-    winston.error('Error creating ipa', err);
-    process.exit(1);
-});
+create()
+    .catch((err) => {
+        winston.error('Error creating ipa', err);
+        process.exit(1);
+    });
