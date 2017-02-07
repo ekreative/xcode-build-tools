862b21aae46fbd57e8e4a33682bb18c8b4faca4e
Fred Cox <mcfedr@gmail.com>
2017-02-07 14:59:54 +0200

    Add a script to set dev version

---
+++ dev-version-xcode.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var winston = require('winston'),
+    program = require('commander'),
+
+    exec = require('./lib/exec');
+
+program.version(require('./package.json').version)
+    .description('Set version for building')
+    .option('-v, --current-version <current-version>', 'Current main version')
+    .parse(process.argv);
+
+var version = function() {
+    return Promise.resolve()
+        .then(function() {
+            if (program.currentVersion) {
+                return program.currentVersion;
+            } else {
+                return exec('agvtool what-marketing-version -terse1')
+                    .then(function(result) {
+                        return result.stdout.trim();
+                    })
+            }
+        })
+        .then(function(version) {
+            return exec('agvtool new-version -all "' + version + 'dev$(date +"%Y_%m_%d_%H_%M")"')
+        });
+};
+
+version().catch(function (err) {
+    winston.error('Error updating version', err);
+    process.exit(1);
+});
