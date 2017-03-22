2dc98f7bb80d098923f9b28b29f5eb30cd9a6dc1
Fred Cox <mcfedr@gmail.com>
2017-03-22 13:46:57 +0200

    Add create-release

---
+++ create-release.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var child_process = require('child_process'),
+    fetch = require('node-fetch'),
+    FormData = require('form-data'),
+    program = require('commander'),
+    winston = require('winston');
+
+program
+    .version(require('./package.json').version)
+    .description('Create a new release on GitLab')
+    .option('--server <server>', 'GitLab server', process.env.GITLAB_API || 'https://git.ekreative.com')
+    .option('--token <token>', 'Api key', process.env.GITLAB_API_TOKEN)
+    .option('--project-id <id>', 'Project Id', process.env.CI_PROJECT_ID)
+    .option('--tag-name <tag>', 'Tag name')
+    .option('--ref <ref>', 'Git ref', process.env.CI_COMMIT_SHA || process.env.CI_BUILD_REF)
+    .option('-n, --notes <notes>', 'Release notes', child_process.execSync('git log --format=%B -n 1 || echo "No comment"'))
+    .parse(process.argv);
+
+winston.info('Creating release');
+
+var data = new FormData();
+data.append('tag_name', program.tagName);
+data.append('ref', program.ref);
+data.append('release_description', program.notes);
+
+data.getLengthSync = null; //Work around until https://github.com/bitinn/node-fetch/issues/102
+
+var result = fetch(program.server + '/projects/' + program.projectId + '/tags', {
+    method: 'POST',
+    body: data,
+    headers: {
+        'PRIVATE-TOKEN ': program.token
+    }
+})
+    .then(function (res) {
+        if (res.status == 200) {
+            return res;
+        }
+        return res.text().then(function (body) {
+            throw new Error('Failed to create release [' + body + ']');
+        });
+    });
+
+result.catch(function (err) {
+    winston.error('Error creating release', err);
+    process.exit(1);
+});
