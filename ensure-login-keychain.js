87a7ae23bb1a3303bb46997e82ea11c97e88ad80
Fred Cox <frederick.j.cox@gmail.com>
2017-11-22 16:28:49 +0200

    Remove unused imports

--- ensure-login-keychain.js
+++ ensure-login-keychain.js
@@ -2,37 +2,32 @@
 
 'use strict'
 
-var cpr = require('cpr')
-var crypto = require('crypto')
-var os = require('os')
-var path = require('path')
 var program = require('commander')
 var winston = require('winston')
 
 var exec = require('./lib/exec')
-var list = require('./lib/list')
 
 program
   .version(require('./package.json').version)
   .description('Ensure default login keychain exists')
   .parse(process.argv)
 
-exec('security list-keychains -d user').then(function(process) {
+exec('security list-keychains -d user').then(function (process) {
   if (/keychain/.test(process.stdout)) {
     winston.info('Keychain exists')
   } else {
-    return exec('security create-keychain -p "" login.keychain').then(function(process) {
+    return exec('security create-keychain -p "" login.keychain').then(function (process) {
       winston.info('Created keychain')
-    }).catch(function(err) {
+    }).catch(function (err) {
       if (err === 48) {
         winston.info('Restored keychain')
       } else {
-        throw err;
+        throw err
       }
     })
   }
 })
-  .catch(function(err) {
-    winston.error('Tried to create keychain', err);
+  .catch(function (err) {
+    winston.error('Tried to create keychain', err)
     process.exit(1)
   })
